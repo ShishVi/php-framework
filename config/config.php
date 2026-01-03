c9b857baa93bf54aff34cc94619e30a25026351e
Vitaly Shishov <shishov_84@mail.ru>
2026-01-03 14:24:56 +0300

    init project

--- config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define("ROOT", dirname(__DIR__));
-
-const CONFIG = ROOT . '/config';
-
-?>
--- public/index.php
+++ public/index.php
@@ -1,37 +1,10 @@
 <?php
 
-use App\Controllers\HomeController;
-use coreFramework\Application;
+$start_frame = microtime(true);
 
-$start_time = microtime(true);
+if(PHP_MAJOR_VERSION < 8)
+{
+    die('Require PHP version >= 8');
+}
 
-
-if(PHP_MAJOR_VERSION < 8) {
-    die('Require PHP 8 or higher');
-};
-
-require_once __DIR__.'/../config/config.php';
-require_once ROOT. '/vendor/autoload.php';
-require_once ROOT. '/helpers/helpers.php';
-
-
-$app = new Application;
-
-require_once CONFIG. '/routes.php';
-
-$app->run();
-
-/* dump($app);
-dump(request()->getMethod());
-dump($app->request->isGet());
-dump($app->request->isPost());
-dump($app->request->isAjax());
-dump(request()->get('user'));
-dump($app->request->get('age'));
-dump($app->request->post('user'));
-dump(request()->get('users'));
- */
-
-dump("Time: " . (microtime(true) - $start_time));
-
-?>
+var_dump("Time ". microtime(true) - $start_frame);
